f2b1b093176a98fe617632fb00da7e48224c05fc
Esa Karjalainen <esa.karjalainen@gmail.com>
2010-09-15 22:52:53 +0300

    vaihto PHPhen alkaa

--- make_thumbnail.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-echo "tehd‰‰n kuvakkeet"
-CONVERT="E:\Tools\ImageMagick-6.6.0-Q8\convert"
-cd images_in
-for file in *.jpg do $CONVERT -define jpeg:size=200x200 %%a -thumbnail 115x110 -background black -gravity center -extent 115x110 ..\baked\gallery\thumbnails\tn_%%a
-cd ..
-pause
--- make_webimages.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-echo tehdaan web-kokoiset kuvat...
-cd images_in
-E:\Tools\ImageMagick-6.6.0-Q8\mogrify -format jpg *
-for %%a in (*.jpg) do E:\Tools\ImageMagick-6.6.0-Q8\convert -define jpeg:size=800x600 %%a -resize 800x600 -background black -gravity center -extent 800x600 ..\baked\gallery\%%a
-cd ..
