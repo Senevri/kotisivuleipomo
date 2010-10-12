943438e7d64fa87fcc0686d64859555b23f5e711
Esa Karjalainen <esa.karjalainen@gmail.com>
2010-10-12 09:14:36 +0300

    added static + templates to repo

---
+++ templates/gallery_view.tpl.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//w3c//dtd html 4.01 transitional//en" "http://www.w3.org/tr/html4/loose.dtd">
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=windows-1252"><title>Galleria</title>
+<link rel="stylesheet" type="text/css" href="../static/juhan-style.css">
+</head>
+<body>
+	<table><tr><td>
+		<!--prev-->
+	</td>
+	<td id="kuva"><img src="kuvat/<!--file-->" /></td>
+	<td>
+		<!--next-->
+	</td></tr></table>
+	<div><div id="kuvateksti"><!--kuvateksti--></div>
+	<a href="..\galleria.html">
+	<img style="border: 0px solid ; width: 176px; height: 64px;" alt="Takaisin" src="../static/takaisin_2.gif">
+	</a>
+	</div>
+</body>
+</html>
+
