19e9dcc129fdde148b76c5004768ae7db35818ee
Esa Karjalainen <esa.karjalainen@gmail.com>
2015-10-09 23:24:14 +0300

    added subdir functionality to gallery generation. Stupid relative paths.

--- scripts/make_gallery.php
+++ scripts/make_gallery.php
@@ -1,29 +1,51 @@
 <?php 
-$subdir="";
+$subdir="alikansio";
 echo "tehdaan galleria\n";
 $imagedir="images_in";
-$imagick='E:\\Tools\\ImageMagick-6.6.0-Q8';
+$imagick='c:\\Apps\\ImageMagick-6.9.2-Q16';
 $cmd = trim($argv[1]);
 if($cmd=="path") {
 	$imagedir = trim($argv[2]);
 	$cmd = trim($argv[3]);
 }
 
+
 $header = file_get_contents("templates\\gallery_header.tpl.html");
 $footer = file_get_contents("templates\\footer.tpl.html");
 $files = scandir($imagedir);
 $tmp = array();
 $col = 0;
-$body .="<!--galleria--><h2 class=\"gallery\">Otsikko</h2><div><i><h4>Paikka ja aika</h4></i><span>tekstiä</span></div><table class=\"gallery\"><tr>";
+$body .= "<!--galleria--><h2 class=\"gallery\">Otsikko</h2><div><i><h4>Paikka ja aika</h4></i><span>tekstiä</span></div>";
+$body .= "<table class=\"gallery\"><tr>";
 foreach($files as $file) {
 	if(strtolower(substr($file, -3, 3))=="jpg") {
 		$tmp[] = str_replace(' ', '_', $file);
 	}
 }
+if ($cmd=="scandir") {
+	echo "kirjoitetaan vain tiedostot tekstifiluun";
+	file_put_contents("files.txt", implode("\n", $tmp));
+	exit();
+}
+if ($cmd=="usefile") {
+	echo "käytetään files.txt:tä tiedostojen järjestykseen.";
+	$tmp = explode("\n", file_get_contents("files.txt"));	
+}
+
+
 $files = $tmp;
+
+foreach($files as $file) {
+	echo $file."\n";
+}
+
+$unixsubdir="";
+if (strlen($subdir)>0) {
+	$unixsubdir=$subdir . "/";	
+}
 foreach($files as $file) {
 	//if(strtolower(substr($file, -3, 3))=="jpg") {
-	$body .= "<td><a href=\"gallery/$file.html\">\n<img src=\"gallery/thumbnails/".$subdir."tn_$file\" /><a></td>\n";
+	$body .= "<td><a href=\"gallery/$unixsubdir$file.html\">\n<img src=\"gallery/" .$unixsubdir. "thumbnails/tn_$file\" /><a></td>\n";
 	$col++;
 	if ($col==4) {
 		$col=0;
@@ -49,24 +71,34 @@ if($cmd!="update") {
 
 $index = 0;
 //$outfile="baked\\gallery\\". date("Y-m-j");
+
 $outfile="baked\\gallery\\";
+if(strlen($subdir)>0) {
+	$outfile .= $subdir . "\\";	
+}
+
 //$header = file_get_contents("templates\\image_header.tpl.html");
 $template = file_get_contents('templates\gallery_view.tpl.php');
 $output = $template;
+
+
 foreach($files as $file ) {
 	if($index>0) {
 		$prev = $index-1;
 		$content = "<a href=\"$files[$prev].html\">";
-		$content .= "<img src=\"thumbnails/tn_$files[$prev]\" alt=\"edellinen\" /></a>";
+		$content .= "<img src=\"thumbnails/" . "tn_$files[$prev]\" alt=\"edellinen\" /></a>";
 		$output = str_replace('<!--prev-->', $content, $template);
 	}
 	$output = str_replace('<!--file-->', $file, $output);
 	if($index!=(count($files)-1) ) {
 		$next = $index+1;
 		$content = "<a href=\"$files[$next].html\">";
-		$content .= "<img src=\"thumbnails/tn_$files[$next]\" alt = \"seuraava\" /></a>";
+		$content .= "<img src=\"thumbnails/" . "tn_$files[$next]\" alt = \"seuraava\" /></a>";
 		$output = str_replace('<!--next-->', $content, $output);
 	}
+	//if (strlen($subdir) > 0) {
+	//	$output = str_replace("{{subdir}}",  , $output);
+	//}
 	file_put_contents($outfile . $file . ".html", $output);
 	$index++;
 }	
--- scripts/make_images.php
+++ scripts/make_images.php
@@ -1,8 +1,8 @@
 <?php 
-$subdir="";
+$subdir="alikansio";
 echo "tehdaan web-kokoiset kuvat...\n";
 $imagedir="images_in";
-$imagick='E:\\Tools\\ImageMagick-6.6.0-Q8';
+$imagick='c:\\Apps\\ImageMagick-6.9.2-Q16';
 $cmd = trim($argv[1]);
 
 if($cmd=="path"){
@@ -18,14 +18,23 @@ foreach($files as $file) {
 	}
 }
 $files = $tmp; 
+$unixsubdir="";
+if (strlen($subdir)>0) {
+	$unixsubdir=$subdir . "\\";	
+	mkdir("baked\\gallery\\". $unixsubdir);
+	mkdir("baked\\gallery\\". $unixsubdir . "kuvat\\");
+	mkdir("baked\\gallery\\" . $unixsubdir . "thumbnails\\");
+	
+}
+
 foreach($files as $file ) {
 	printf("$file\n");
 	//	printf("$imagick\\convert -define jpeg:size=800x600 \"$imagedir\\$file\" -resize 800x600 -background black -gravity center -extent 800x600 baked\\gallery\\". str_replace(' ', '_', $file));
 	if($cmd!="only_tn"){
-		system("$imagick\\convert -define jpeg:size=800x600 \"$imagedir\\$file\" -resize 800x600 -background black -gravity center -extent 800x600 baked\\gallery\\kuvat\\". $subdir . str_replace(' ', '_', $file));
+		system("$imagick\\convert -define jpeg:size=800x600 \"$imagedir\\$file\" -resize 800x600 -background black -gravity center -extent 800x600 baked\\gallery\\". $unixsubdir . "kuvat\\" . str_replace(' ', '_', $file));
 	}
 	if($cmd!="no_tn") {	
-		system("$imagick\\convert -define jpeg:size=200x200 \"$imagedir\\$file\" -thumbnail 115x110 -background black -gravity center -extent 115x110 baked\\gallery\\thumbnails\\tn_" . $subdir . str_replace(' ', '_', $file));
+		system("$imagick\\convert -define jpeg:size=200x200 \"$imagedir\\$file\" -thumbnail 115x110 -background black -gravity center -extent 115x110 baked\\gallery\\" . $unixsubdir . "thumbnails\\" . "tn_" . str_replace(' ', '_', $file));
 	}
 }	
 ?>
